33a8a50e9ba479bc3af1ce6e7cb81de45d12d068
sidney <shideneyu@gmail.com>
2014-05-24 14:41:17 +0200

    The chosen servant is now tested, picked and removed from the hand

--- main.py
+++ main.py
@@ -9,11 +9,12 @@ player_two = Player("yassine")
 
 # Create the servants
 dieudonne = Servant("Dieudonne", "42", "80", "100")
+
+
 taubira = Servant("Taubira", "30", "70", "80")
 
 # Set the servants inside an array
 servants = [dieudonne, taubira]
-
 # Initialize variables
 current_player = player_two
 current_opponent = player_one
@@ -34,14 +35,14 @@ while(current_player.health_point != 0):
   print("----------------- TURN " + str(turn_count) + " -----------------\n\n")
   print(current_player.name +"'s turn. You have " + str(timer) + " seconds! Choose a card between:")
   # Print every servant card in player's hand
-  for servant in servants:
-    print("------->" + servant.name)
+  for servant_name in list(map(lambda x: x.name, servants)):
+    print("------->" + servant_name)
   print('\n')
   record = input("What is your choice?\n")
 
-
   # If inputed answer is in the player's hand 
-  if (record):
+  if (record in list(map(lambda x: x.name, servants))):
+    servants = [servant for servant in servants if servant.name != record]
     turn_count+=1
     os.system('cls' if os.name == 'nt' else 'clear')
     history = history + current_opponent.name + " chose " + record + "\n"
